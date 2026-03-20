a58c8ad1c01520fc3d8e877247a2efdf986b9e49
Ilya Virchenko <93025225+XZcov@users.noreply.github.com>
2026-03-20 10:48:39 +0200

    Доповнив модуль для лаб9

--- lab08/prj/ModulesВірченко/s_calculation.h
+++ lab08/prj/ModulesВірченко/s_calculation.h
@@ -1,6 +1,18 @@
 #ifndef S_CALCULATION_H
 #define S_CALCULATION_H
 
+#include <string>
+#include <vector>
+
 double s_calculation(double x, double y, double z);
+
+// Задача 9.1
+void wind_calculation(int b, std::string& speed, std::string& description);
+
+// Задача 9.2
+void power_and_odds(int n, int m, long long& n_pow_m, long long& m_pow_n, std::vector<int>& odds);
+
+// Задача 9.3
+int binary_analysis(int N);
 
 #endif
